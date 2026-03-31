b975a56411238d717fd87510ba3a94190ed9e46a
pashahsjsjsk <167359190+pashahsjsjsk@users.noreply.github.com>
2026-03-31 09:58:21 +0300

    Initial Android Studio project
    Initial Android Studio project

---
+++ app/src/main/java/com/example/receiptapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.receiptapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
